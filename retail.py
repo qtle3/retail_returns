e9c2c04f3e89846299c7fe3ba08cf2dbc585ab09
Quocviet Le <quocvietlet@gmail.com>
2024-08-08 20:44:49 -0500

    format change

--- retail.py
+++ retail.py
@@ -1,20 +1,24 @@
-#retail store return policy
+# retail store return policy
+
 
 def main():
     print("This program if you are eligible to return your item at the store.")
+    # ask the user if they have a receipt
     receipt = input("Do you have your reciept: yes/no? ")
     if receipt == "no":
         print("You cannot return your item.")
-    else:
-        days = input("Has it been past 90 days: yes/no? ")
-        if days == "yes":
-            print("You cannot return your item.")
-        else:
-            food = input("is it a food item: yes/no: ")
-            if food == "yes":
-                print("You cannot return your item.")
-            else:
-                print("You can return your item!")
-main()        
-    
-    
+        return
+    days = input("Has it been past 90 days: yes/no? ")
+    if days == "yes":
+        print("You cannot return your item.")
+        return
+
+    food = input("is it a food item: yes/no: ")
+    if food == "yes":
+        print("You cannot return your item.")
+        return
+
+    print("You can return your item!")
+
+
+main()
